af1e8f207657db0be125dbe85a54b85c30d6e0fe
MonoLogueCHi <xxwhite.com@gmail.com>
2019-08-15 23:18:22 +0800

    提高历史弹幕查询效率

--- Danmaku/Utils/BiliBili/BilibiliHelp.cs
+++ Danmaku/Utils/BiliBili/BilibiliHelp.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Xml;
 using Danmaku.Model;
 using Microsoft.Extensions.Configuration;
@@ -19,7 +21,7 @@ namespace Danmaku.Utils.BiliBili
         }
 
         /// <summary>
-        /// 获得cid
+        ///     获得cid
         /// </summary>
         /// <param name="aid">aid</param>
         /// <param name="p">p</param>
@@ -49,89 +51,69 @@ namespace Danmaku.Utils.BiliBili
         }
 
         /// <summary>
-        /// 使用cid获得弹幕列表
+        ///     使用cid获得弹幕列表
         /// </summary>
         /// <param name="cid"></param>
         /// <returns></returns>
         public List<DanmakuGet> GetBiDanmaku(string cid)
         {
-            try
-            {
-                using (var client = new WebClient())
-                {
-                    var gResult = client.DownloadDataTaskAsync($"https://api.bilibili.com/x/v1/dm/list.so?oid={cid}")
-                        .Result;
-                    var result = DeDeflate(gResult);
-                    var xml = new XmlDocument();
-                    xml.LoadXml(result);
-                    var ds = xml.GetElementsByTagName("d");
-                    var dgs = new List<DanmakuGet>();
-                    foreach (XmlNode d in ds)
-                    {
-                        var dg = new DanmakuGet();
-                        var ps = d.Attributes["p"].InnerText.Split(",");
-                        dg.Time = float.Parse(ps[0]);
-                        dg.Color = int.Parse(ps[3]);
-                        dg.Type = int.Parse(ps[5]);
-                        dg.Author = "";
-                        dg.Text = d.InnerText;
-                        dgs.Add(dg);
-                    }
-
-                    return dgs;
-                }
-            }
-            catch
-            {
-                return new List<DanmakuGet>();
-            }
+            return GetBiDanmakuDataAsync($"https://api.bilibili.com/x/v1/dm/list.so?oid={cid}").Result;
         }
 
         /// <summary>
-        /// 使用cid获得历史弹幕
+        ///     使用cid获得历史弹幕
         /// </summary>
         /// <param name="cid"></param>
         /// <param name="date"></param>
         /// <returns></returns>
         public List<DanmakuGet> GetBiDanmaku(string cid, string[] date)
         {
-            try
+            var dgss = new List<Task<List<DanmakuGet>>>();
+            foreach (var da in date)
             {
-                using (var client = new WebClient())
-                {
-                    client.Headers.Set("Cookie", _appConfiguration.BCookie);
-                    var dgs = new List<DanmakuGet>();
-                    foreach (var da in date)
-                    {
-                        var gResult = client
-                            .DownloadDataTaskAsync(
-                                $"https://api.bilibili.com/x/v2/dm/history?type=1&oid={cid}&date={da}").Result;
-                        var result = DeDeflate(gResult);
-                        var xml = new XmlDocument();
-                        xml.LoadXml(result);
-                        var ds = xml.GetElementsByTagName("d");
-                        //var dgs = new List<DanmakuGet>();
-                        foreach (XmlNode d in ds)
-                        {
-                            var dg = new DanmakuGet();
-                            var ps = d.Attributes["p"].InnerText.Split(",");
-                            dg.Time = float.Parse(ps[0]);
-                            dg.Color = int.Parse(ps[3]);
-                            dg.Type = int.Parse(ps[5]);
-                            dg.Author = "";
-                            dg.Text = d.InnerText;
-                            dgs.Add(dg);
-                        }
-                    }
-
-                    return dgs;
-                }
+                var b = GetBiDanmakuDataAsync(
+                    $"https://api.bilibili.com/x/v2/dm/history?type=1&oid={cid}&date={da}");
+                dgss.Add(b);
             }
-            catch
+            var dgs = new List<DanmakuGet>();
+            foreach (var dg in dgss) dgs = dgs.Concat(dg.Result).ToList();
+
+            return dgs;
+        }
+
+        /// <summary>
+        ///     获取弹幕
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private async Task<List<DanmakuGet>> GetBiDanmakuDataAsync(string url)
+        {
+            using (var client = new WebClient())
             {
-                return new List<DanmakuGet>();
+                if (!string.IsNullOrWhiteSpace(_appConfiguration.BCookie))
+                    client.Headers.Set("Cookie", _appConfiguration.BCookie);
+                var gResult = await client.DownloadDataTaskAsync(url);
+                var result = DeDeflate(gResult);
+                var xml = new XmlDocument();
+                xml.LoadXml(result);
+                var ds = xml.GetElementsByTagName("d");
+                var dgs = new List<DanmakuGet>();
+                foreach (XmlNode d in ds)
+                {
+                    var dg = new DanmakuGet();
+                    var ps = d.Attributes["p"].InnerText.Split(",");
+                    dg.Time = float.Parse(ps[0]);
+                    dg.Color = int.Parse(ps[3]);
+                    dg.Type = int.Parse(ps[5]);
+                    dg.Author = "";
+                    dg.Text = d.InnerText;
+                    dgs.Add(dg);
+                }
+
+                return dgs;
             }
         }
+
 
         /// <summary>
         ///     gzip解压
