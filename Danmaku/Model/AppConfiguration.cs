5a346aca65134ef0676aae047dedd98a911c07f8
MonoLogueChi <xxwhite.com@gmail.com>
2019-10-11 10:53:42 +0800

    删除不必要的配置

--- Danmaku/Model/AppConfiguration.cs
+++ Danmaku/Model/AppConfiguration.cs
@@ -7,33 +7,17 @@ namespace Danmaku.Model
     {
         public static AppConfiguration Config { get; set; }
 
-        public AppConfiguration(IConfiguration configuration)
-        {
-            configuration.Bind(this);
-        }
+        public AppConfiguration(IConfiguration configuration) => configuration.Bind(this);
 
-        public Logging Logging { get; set; }
-
-        public string AllowedHosts { get; set; }
         public string[] WithOrigins { get; set; }
-        public int WebSocketPort { get; set; }
         public DanmakuSQL DanmakuSQL { get; set; }
         public Admin Admin { get; set; }
         public string BCookie { get; set; }
     }
 
-
-    public class Logging
-    {
-        public class LogLevel
-        {
-            public string Default { get; set; }
-        }
-    }
-
     public class DanmakuSQL
     {
-        public SqlType Sql { get; set; } = 0;
+        public SqlType Sql { get; set; } = SqlType.SQLite;
         public string Host { get; set; } = "127.0.0.1";
         public int Port { get; set; } = 0;
         public string UserName { get; set; }
@@ -46,7 +30,8 @@ namespace Danmaku.Model
     {
         PostgreSQL = 0,
         MySQL = 1,
-        SQLite = 2
+        SQLite = 2,
+        SQLServer = 3
     }
 
     public class Admin
