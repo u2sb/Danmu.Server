944055a44963d432e7234de5cf2f210f06f5b2f4
MonoLogueChi <xxwhite.com@gmail.com>
2019-10-22 11:41:11 +0800

    优化内存占用

--- Danmaku/Controllers/Dplayer/V3/BiliBiliController.cs
+++ Danmaku/Controllers/Dplayer/V3/BiliBiliController.cs
@@ -5,38 +5,40 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Danmaku.Controllers.Dplayer.V3
 {
-    [Route("api/dplayer/v3")]
-    [ApiController]
-    public class BiliBiliController : ControllerBase
-    {
-        private readonly IBiliBiliHelp _biliBili;
+	[Route("api/dplayer/v3")]
+	[ApiController]
+	public class BiliBiliController : ControllerBase
+	{
+		private readonly IBiliBiliHelp _biliBili;
 
-        public BiliBiliController(IBiliBiliHelp biliBiliHelp)
-        {
-            _biliBili = biliBiliHelp;
-        }
+		public BiliBiliController(IBiliBiliHelp biliBiliHelp)
+		{
+			_biliBili = biliBiliHelp;
+		}
 
-        // GET: api/dplayer/v3/bilibili
-        [HttpGet("bilibili")]
-        public async Task<string> Get()
-        {
-            var request = Request.Query;
+		// GET: api/dplayer/v3/bilibili
+		[HttpGet("bilibili")]
+		public async Task<string> Get()
+		{
+			var request = Request.Query;
 
-            string[] date = request["date"];
-            var cid = request["cid"].ToString();
-            if (string.IsNullOrEmpty(cid))
-            {
-                var aid = request["aid"];
-                string p = request["p"];
-                p = string.IsNullOrEmpty(p) ? "1" : p;
-                if (!int.TryParse(p, out var page)) return new DanmakuWebResult(1);
-                cid = (await _biliBili.GetCid(aid, page)).ToString();
-            }
-            return string.IsNullOrWhiteSpace(cid)
-                ? new DanmakuWebResult(1)
-                : date.Length == 0
-                    ? new DanmakuWebResult(await _biliBili.GetBiDanmaku(cid))
-                    : new DanmakuWebResult(await _biliBili.GetBiDanmaku(cid, date));
-        }
-    }
+			string[] date = request["date"];
+			var cid = request["cid"].ToString();
+			if (string.IsNullOrEmpty(cid))
+			{
+				var aid = request["aid"];
+				if (string.IsNullOrEmpty(aid)) return new DanmakuWebResult(1);
+				string p = request["p"];
+				p = string.IsNullOrEmpty(p) ? "1" : p;
+				if (!int.TryParse(p, out var page)) return new DanmakuWebResult(1);
+				cid = (await _biliBili.GetCid(aid, page)).ToString();
+			}
+
+			return string.IsNullOrWhiteSpace(cid)
+				? new DanmakuWebResult(1)
+				: date.Length == 0
+					? new DanmakuWebResult(await _biliBili.GetBiDanmaku(cid))
+					: new DanmakuWebResult(await _biliBili.GetBiDanmaku(cid, date));
+		}
+	}
 }
--- Danmaku/Utils/BiliBili/BilibiliHelp.cs
+++ Danmaku/Utils/BiliBili/BilibiliHelp.cs
@@ -9,96 +9,75 @@ using Danmaku.Utils.AppConfiguration;
 
 namespace Danmaku.Utils.BiliBili
 {
-    public class BiliBiliHelp : IBiliBiliHelp
-    {
-        private readonly AppSettings _settings;
-        private readonly IHttpClientFactory _httpClientFactory;
+	public class BiliBiliHelp : IBiliBiliHelp
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly AppSettings _settings;
 
-        public BiliBiliHelp(IAppConfiguration appConfiguration, IHttpClientFactory httpClientFactory)
-        {
-            _settings = appConfiguration.GetAppSetting();
-            _httpClientFactory = httpClientFactory;
-        }
+		public BiliBiliHelp(IAppConfiguration appConfiguration, IHttpClientFactory httpClientFactory)
+		{
+			_settings = appConfiguration.GetAppSetting();
+			_httpClientFactory = httpClientFactory;
+		}
 
-        public async Task<List<BilibiliPage>> GetBilibiliPage(string aid)
-        {
-            var client = _httpClientFactory.CreateClient("gzip");
-            var result = await client.GetStringAsync($"https://www.bilibili.com/widget/getPageList?aid={aid}");
-            var pages = JsonSerializer.Deserialize<List<BilibiliPage>>(result);
-            return pages;
-        }
+		public async Task<List<BilibiliPage>> GetBilibiliPage(string aid)
+		{
+			var client = _httpClientFactory.CreateClient("gzip");
+			var result = await client.GetStringAsync($"https://www.bilibili.com/widget/getPageList?aid={aid}");
+			var pages = JsonSerializer.Deserialize<List<BilibiliPage>>(result);
+			return pages;
+		}
 
-        public int GetCid(List<BilibiliPage> pages, int p)
-        {
-            var cid = 0;
-            foreach (var page in pages)
-                if (page.Page == p)
-                    cid = page.Cid;
-            return cid;
-        }
+		public int GetCid(List<BilibiliPage> pages, int p)
+		{
+			var cid = 0;
+			foreach (var page in pages)
+				if (page.Page == p)
+					cid = page.Cid;
+			return cid;
+		}
 
-        public async Task<int> GetCid(string aid, int p)
-        {
-            var pages = await GetBilibiliPage(aid);
-            return GetCid(pages, p);
-        }
+		public async Task<int> GetCid(string aid, int p)
+		{
+			var pages = await GetBilibiliPage(aid);
+			return GetCid(pages, p);
+		}
 
-        public async Task<List<DanmakuData>> GetBiDanmaku(string cid)
-        {
-            return await GetBiDanmakuDataAsync($"https://api.bilibili.com/x/v1/dm/list.so?oid={cid}", null);
-        }
+		public async Task<List<DanmakuData>> GetBiDanmaku(string cid)
+		{
+			return (await GetBiDanmakuDataAsync($"https://api.bilibili.com/x/v1/dm/list.so?oid={cid}", null)).ToList();
+		}
 
-        public async Task<List<DanmakuData>> GetBiDanmaku(string cid, string[] date)
-        {
-            var dgss = new List<List<DanmakuData>>();
-            foreach (var da in date)
-            {
-                var b = await GetBiDanmakuDataAsync(
-                    $"https://api.bilibili.com/x/v2/dm/history?type=1&oid={cid}&date={da}",
-                    _settings.BCookie);
-                dgss.Add(b);
-            }
+		public async Task<List<DanmakuData>> GetBiDanmaku(string cid, string[] date)
+		{
+			return await Task.Run(() => date.Select(async s => await GetBiDanmakuDataAsync(
+				$"https://api.bilibili.com/x/v2/dm/history?type=1&oid={cid}&date={s}",
+				_settings.BCookie)).SelectMany(s => s.Result).ToList());
+		}
 
-            var dgs = new List<DanmakuData>();
-            foreach (var dg in dgss) dgs = dgs.Concat(dg).ToList();
-            return dgs;
-        }
+		private async Task<IEnumerable<DanmakuData>> GetBiDanmakuDataAsync(string url, string cookie)
+		{
+			var client = _httpClientFactory.CreateClient("deflate");
+			if (!string.IsNullOrEmpty(cookie)) client.DefaultRequestHeaders.Add("Cookie", cookie);
+			var result = await client.GetStringAsync(url);
 
-        private async Task<List<DanmakuData>> GetBiDanmakuDataAsync(string url, string cookie)
-        {
-            try
-            {
-                var client = _httpClientFactory.CreateClient("deflate");
-                if (!string.IsNullOrEmpty(cookie)) client.DefaultRequestHeaders.Add("Cookie", cookie);
-                var result = await client.GetStringAsync(url);
+			var xml = new XmlDocument();
+			xml.LoadXml(result);
+			var ds = xml.GetElementsByTagName("d");
+			if (ds.Count == 0) return null;
 
-                var xml = new XmlDocument();
-                xml.LoadXml(result);
-                var ds = xml.GetElementsByTagName("d");
-
-                var dgs = new List<DanmakuData>();
-                if (ds.Count == 0) return dgs;
-
-                foreach (XmlNode d in ds)
-                {
-                    var ps = d.Attributes["p"].InnerText.Split(",");
-                    var dg = new DanmakuData
-                    {
-                        Time = float.Parse(ps[0]),
-                        Color = int.Parse(ps[3]),
-                        Type = int.Parse(ps[5]),
-                        Author = "",
-                        Text = d.InnerText
-                    };
-                    dgs.Add(dg);
-                }
-
-                return dgs;
-            }
-            catch
-            {
-                return new List<DanmakuData>();
-            }
-        }
-    }
+			return ds.Cast<XmlNode>().Select(s =>
+			{
+				var d = s.Attributes["p"].InnerText.Split(",");
+				return new DanmakuData
+				{
+					Time = float.Parse(d[0]),
+					Color = int.Parse(d[3]),
+					Type = int.Parse(d[5]),
+					Author = "",
+					Text = s.InnerText
+				};
+			});
+		}
+	}
 }
