61970293eea18a2167f4b5019f313b500ca636d3
MonoLogue Chi <xxwhite.com@gmail.com>
2020-02-18 15:11:09 +0800

    添加artplayer弹幕提交功能

--- Danmaku/Controllers/ArtPlayer/V1/ArtPlayerController.cs
+++ Danmaku/Controllers/ArtPlayer/V1/ArtPlayerController.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Danmaku.Controllers.Base;
 using Danmaku.Model;
@@ -22,6 +24,21 @@ namespace Danmaku.Controllers.ArtPlayer.V1
         public async Task<string> Get(string id)
         {
             return string.IsNullOrEmpty(id) ? null : new BilibiliDanmakuData(await Dao.DanmakuQuery(id)).ToXml();
+        }
+
+        // POST: api/artplayer/v1/
+        [HttpPost]
+        public async Task<string> Post([FromBody] DanmakuDataInsert data)
+        {
+            if (string.IsNullOrWhiteSpace(data.Id) || string.IsNullOrWhiteSpace(data.Text))
+                return new DanmakuWebResult(1);
+            data.Ip = IPAddress.TryParse(Request.Headers["X-Real-IP"], out var ip)
+                    ? ip
+                    : Request.HttpContext.Connection.RemoteIpAddress;
+            data.Referer = Request.Headers["Referer"].FirstOrDefault();
+
+            var result = await Dao.DanmakuInsert(data);
+            return result ? new DanmakuWebResult(0) : new DanmakuWebResult(1);
         }
     }
 }
