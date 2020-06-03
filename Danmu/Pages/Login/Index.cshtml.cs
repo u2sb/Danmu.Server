62dbe2d0d0b6c631ca19d4f00cfad51e47c9963d
MonoLogue Chi <xxwhite.com@gmail.com>
2020-06-03 17:29:32 +0800

    更新登录页

---
+++ Danmu/Pages/Login/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Danmu.Pages.Login
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
